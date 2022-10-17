d33bd5351c5079007cf728a438497f438ea25cc5
Tomas Pilny <tomas.pilny@espressif.com>
2022-10-17 13:39:23 +0200

    Added color matching audio feedback

--- memorygame/memorygame.ino
+++ memorygame/memorygame.ino
@@ -1,9 +1,10 @@
 #include <Bounce2.h>
 #include "tones.h"
-#include "I2S.h"
-#include "start_1.c"
-#include "lvl_up_1.c"
-#include "fail_1.c"
+
+//#include "I2S.h"
+//#include "start_1.c"
+//#include "lvl_up_1.c"
+//#include "fail_1.c"
 
 // Pinout set for ESP32-C3 DevkitC
 // Connect with S2 Kaluga via UART:
@@ -28,11 +29,11 @@ uint8_t led_pin_map[4] = {RED_LED, GREEN_LED, BLUE_LED, YELLOW_LED};
 char led_text_map[4][14] = {"RED_LED", "GREEN_LED", "BLUE_LED", "YELLOW_LED"};
 
 enum command {RESTART, STARTUP, LEVEL_UP, FAILED};
-enum buzzer_msg {PLAY_START, PLAY_SUCCESS, PLAY_LVL_UP, PLAY_FAIL};
+enum buzzer_msg {PLAY_START, PLAY_SUCCESS, PLAY_LVL_UP, PLAY_FAIL, PLAY_RED, PLAY_GREEN, PLAY_BLUE, PLAY_YELLOW};
 
 // GPIO 8 is for RGD LED on board
 
-#define BUZZER_PIN 9 // PDM
+#define BUZZER_PIN 9
 
 #define MAX_GAME_SEQUENCE 30
 #define FLASH_DELAY 250
@@ -92,17 +93,35 @@ void fail_strobe(){
   }
 }
 
-void play_start(){
-  log_d("play start (but not really)");
-  delay(2000);
-  /*
-  if(I2S.begin(PDM_MONO_MODE, start_1_sampling_frequency, start_1_bits_per_sample)){
-    I2S.setDataInPin(BUZZER_PIN);
-    I2S.write(start_1_samples, start_1_size);
-    I2S.end();
-  }
-  */
 /*
+// I2S function
+void play_array(const uint32_t sampling_frequency,
+                const uint8_t bits_per_sample,
+                const size_t size,
+                const int16_t *samples){
+  log_d("play array");
+  if(I2S.begin(PDM_STEREO_MODE, sampling_frequency, bits_per_sample)){
+    size_t written = 0;
+    int pointer = 0;
+    int available = 0;
+    I2S.setSckPin(PDM_CLK);
+    I2S.setDataInPin(BUZZER_PIN);
+    while(pointer < size){
+      available = I2S.availableForWrite();
+      if(available){
+        written = I2S.write((void*)(samples+pointer), available);
+        pointer += written;
+        log_d("availableForWrite = %d; written = %d; new pointer = %d", available, written, pointer);
+      }
+    } // while data remain to be played
+    I2S.end();
+  }else{log_e("Failed to start I2S in PDM mode");}
+}
+*/
+
+void play_start(){
+  //play_array(start_1_sampling_frequency, start_1_bits_per_sample, start_1_size, start_1_samples);
+  delay(150);
   tone(BUZZER_PIN, NOTE_D3, 150);
   delay(150);
   tone(BUZZER_PIN, NOTE_E3, 150);
@@ -110,24 +129,17 @@ void play_start(){
   tone(BUZZER_PIN, NOTE_F3, 150);
   delay(150);
   tone(BUZZER_PIN, NOTE_G3, 300);
-*/
+  delay(150);
 }
 
 void play_success(){ // play after each successful button press (not necessary)
   //tone(BUZZER_PIN, NOTE_A3, 250);
+  //delay(150);
 }
 
 void play_lvl_up(){
-  log_d("play lvl up (but not really)");
-  delay(2000);
-  /*
-  if(I2S.begin(PDM_MONO_MODE, lvl_up_1_sampling_frequency, lvl_up_1_bits_per_sample)){
-    I2S.setDataInPin(BUZZER_PIN);
-    I2S.write(lvl_up_1_samples, lvl_up_1_size);
-    I2S.end();
-  }
-  */
-/*
+  //play_array(lvl_up_1_sampling_frequency, lvl_up_1_bits_per_sample, lvl_up_1_size, lvl_up_1_samples);
+  delay(250);
   tone(BUZZER_PIN, NOTE_B3, 150);
   delay(150);
   tone(BUZZER_PIN, NOTE_C4, 150);
@@ -135,21 +147,14 @@ void play_lvl_up(){
   tone(BUZZER_PIN, NOTE_D4, 150);
   delay(150);
   tone(BUZZER_PIN, NOTE_E4, 200);
-  */
+  delay(150);
 }
 
 void play_fail(){
-  log_d("play fail (but not really)");
-  delay(2000);
-  /*
-  if(I2S.begin(PDM_MONO_MODE, fail_1_sampling_frequency, fail_1_bits_per_sample)){
-    I2S.setDataInPin(BUZZER_PIN);
-    I2S.write(fail_1_samples, fail_1_size);
-    I2S.end();
-  }
-  */
-  /*
+  //play_array(fail_1_sampling_frequency, fail_1_bits_per_sample, fail_1_size, fail_1_samples);
+
   // tu-du-du-duuu
+  delay(150);
   tone(BUZZER_PIN, NOTE_G3, 150);
   delay(150);
   tone(BUZZER_PIN, NOTE_F3, 150);
@@ -157,11 +162,31 @@ void play_fail(){
   tone(BUZZER_PIN, NOTE_E3, 150);
   delay(150);
   tone(BUZZER_PIN, NOTE_D3, 400);
-  */
+  delay(150);
+}
+
+void play_red(){
+  delay(150);
+  tone(BUZZER_PIN, NOTE_F4, 100);
+  delay(150);
+}
+void play_green(){
+  delay(150);
+  tone(BUZZER_PIN, NOTE_E4, 100);
+  delay(150);
+}
+void play_blue(){
+  delay(150);
+  tone(BUZZER_PIN, NOTE_D4, 100);
+  delay(150);
+}
+void play_yellow(){
+  delay(150);
+  tone(BUZZER_PIN, NOTE_C4, 100);
+  delay(150);
 }
 
 void generate_sequence(unsigned short *sequence, unsigned long max_game_sequence){
-  //log_d("gen sequence");
   // Create a new game sequence.
   randomSeed(analogRead(0));
   unsigned short second_prev;
@@ -172,18 +197,16 @@ void generate_sequence(unsigned short *sequence, unsigned long max_game_sequence
   sequence[1] = random(0, 3);
   prev = sequence[1];
 
-  //log_d("start generating");
   for (int i = 2; i < max_game_sequence; i++) {
     sequence[i] = random(0, 3);
     if(sequence[i] == second_prev || sequence[i] == prev){
       // prevent 3 matching colors in a row (2 in row is ok)
       sequence[i] = (sequence[i]+1)%4;
     }
-    Serial.printf("[%d]=%d\n", i, sequence[i]);
+    //Serial.printf("[%d]=%d\n", i, sequence[i]);
     second_prev = prev;
     prev = sequence[i];
   }
-  //log_d("done generating");
 }
 
 void correct_strobe(){
@@ -201,33 +224,34 @@ void correct_strobe(){
 void buzzer_task(void* param){
   while(1){
     uint8_t buzzer_job = 255;
-    //log_d("receiving side - buzzer_job after init %d", buzzer_job);
     if( buzzer_queue != NULL ){
-      //log_d("receiving side - queue exists");
       if(xQueueReceive(buzzer_queue, &buzzer_job, ( TickType_t ) 100 ) == pdPASS){
-        log_d("receiving side - queue received item %d", buzzer_job);
-        //delay(500);
-        //Serial.printf("buzzer task job %d\n", buzzer_job);
-        //delay(500);
         switch(buzzer_job){
           case PLAY_START:
-            log_d("switch: PLAY_START");
             play_start();
             break;
           case PLAY_SUCCESS:
-            log_d("switch: PLAY_SUCCESS");
             play_success();
             break;
           case PLAY_LVL_UP:
-            log_d("switch: PLAY_LVL_UP");
             play_lvl_up();
             break;
           case PLAY_FAIL:
-            log_d("switch: PLAY_FAIL");
             play_fail();
             break;
+          case PLAY_RED:
+            play_red();
+            break;
+          case PLAY_GREEN:
+            play_green();
+            break;
+          case PLAY_BLUE:
+            play_blue();
+            break;
+          case PLAY_YELLOW:
+            play_yellow();
+            break;
           default:
-            log_d("switch: default");
         } // swtich
       } // if xQueueReceive OK
     } // if buzzer_queue exists
@@ -249,8 +273,8 @@ void setup() {
 
   pinMode(BUZZER_PIN, OUTPUT);
 
-  //Serial1.begin(115200);
-  //while(!Serial1){delay(100);}
+  Serial1.begin(115200);
+  while(!Serial1){delay(100);}
 
   buzzer_queue = xQueueCreate(10, sizeof(uint8_t));
   TaskHandle_t buzzer_task_handler = NULL;
@@ -264,14 +288,9 @@ void setup() {
     );
   if(buzzer_task_handler == NULL){
     log_e("Could not create red task");
-    // todo
+    ESP.restart();
   }
-
-  //I2S.begin(PDM_STEREO_MODE, 8000, 16);
-  //I2S.begin(PDM_MONO_MODE, lvl_up_audioSamplingFrequency, 16);
-  //I2S.setDataInPin(BUZZER_PIN);
-
-  //Serial1.write(RESTART);
+  Serial1.write(RESTART);
   flash_timer = millis();
 }
 
@@ -287,14 +306,12 @@ void loop() {
       buzzer_job = PLAY_START;
       if( buzzer_queue != 0 ){
         if(xQueueSend(buzzer_queue, ( void * ) &buzzer_job, ( TickType_t ) 100 ) != pdPASS ){
-          log_d("O-oufailt to push into queue");
-        }else{
-          log_d("ok, pushed to queue %d", buzzer_job);
+          log_w("Failed to push into queue - buzzer will be silent");
         }
       }else{
-        log_d("Oh no, how could this happen buzzer_queue is == 0");
+        log_e("buzzer_queue is == 0");
       }
-      //Serial1.write(STARTUP);
+      Serial1.write(STARTUP);
       generate_sequence(gameSequence, MAX_GAME_SEQUENCE);
 
       currentSequenceLength = 1;
@@ -309,7 +326,7 @@ void loop() {
       if (millis() - flash_timer >= FLASH_DELAY){
         attractLEDOn = ! attractLEDOn;
         if(attractLEDOn){
-          //all_leds_on(); // TODO - uncommnet - this was annoying to work with
+          all_leds_on();
         }else{
           all_leds_off();
         }
@@ -322,6 +339,7 @@ void loop() {
       // Play the pattern out to the user
       for (n = 0; n < currentSequenceLength; n++) {
         Serial.printf("gameSequence[%d] = %d => Flash pin led_pin_map[%d] = %d = \"%s\"\n", n, gameSequence[n], gameSequence[n], led_pin_map[gameSequence[n]], led_text_map[gameSequence[n]]);
+        buzzer_job = PLAY_RED+gameSequence[n]; xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
         led_on(led_pin_map[gameSequence[n]]);
         delay(FLASH_DELAY);
         led_off(led_pin_map[gameSequence[n]]);
@@ -337,25 +355,29 @@ void loop() {
       int userPressed = -1;
 
       if (debouncerRed.fell()) {
-        Serial.println("red");
+        Serial.println("Red");
+        buzzer_job = PLAY_RED; xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
         led_on(RED_LED);
         delay(FLASH_DELAY);
         led_off(RED_LED);
         userPressed = RED_BUTTON;
       } else  if (debouncerGreen.fell()) {
-        Serial.println("green");
+        Serial.println("Green");
+        buzzer_job = PLAY_GREEN; xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
         led_on(GREEN_LED);
         delay(FLASH_DELAY);
         led_off(GREEN_LED);
         userPressed = GREEN_BUTTON;
       } else  if (debouncerBlue.fell()) {
-        Serial.println("blue");
+        Serial.println("Blue");
+        buzzer_job = PLAY_BLUE; xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
         led_on(BLUE_LED);
         delay(FLASH_DELAY);
         led_off(BLUE_LED);
         userPressed = BLUE_BUTTON;
       } else if (debouncerYellow.fell()) {
-        Serial.println("yellow");
+        Serial.println("Yellow");
+        buzzer_job = PLAY_YELLOW; xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
         led_on(YELLOW_LED);
         delay(FLASH_DELAY);
         led_off(YELLOW_LED);
@@ -368,22 +390,21 @@ void loop() {
         if (userPressed != button_pin_map[gameSequence[userPositionInSequence]]) {
           // Failed...
           Serial.println("  Fail");
-          //Serial1.write(FAILED);
+          Serial1.write(FAILED);
           buzzer_job = PLAY_FAIL;
           xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
           fail_strobe();
-          //tone(BUZZER_PIN, NOTE_F3, 300);
-          //delay(300);
-          //tone(BUZZER_PIN, NOTE_G3, 500);
-          //delay(2500);
           gameInProgress = false;
         } else {
           Serial.println("  Correct");
           userPositionInSequence++;
+          buzzer_job = PLAY_SUCCESS;
+          xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
+
           if (userPositionInSequence == currentSequenceLength) {
             buzzer_job = PLAY_LVL_UP;
             xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
-            //Serial1.write(LEVEL_UP);
+            Serial1.write(LEVEL_UP);
             correct_strobe();
 
             // User has successfully repeated back the sequence so make it one longer...
@@ -393,13 +414,6 @@ void loop() {
             if (currentSequenceLength >= MAX_GAME_SEQUENCE) {
               gameInProgress = false;
             }
-
-            // Play a tone...
-            buzzer_job = PLAY_SUCCESS;
-            xQueueSend(buzzer_queue, &buzzer_job, ( TickType_t ) 10 );
-
-            //tone(BUZZER_PIN, NOTE_A3, 300);
-            //delay(2000);
 
             showingSequenceToUser = true;
           }
